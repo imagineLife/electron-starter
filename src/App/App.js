363502c0a3643d219a94574891407288dc7f61d4
Eric Laursen <ericjlaursen@gmail.com>
2020-08-22 11:54:49 -0400

    init "client" react files

---
+++ src/App/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+	<main>
+		<h2>Boilerplate</h2>
+		<p>{`Here, a 'frontend' is setup, utilizing react, webpack, babel, and the star of the party -> electron.`}</p>
+	</main>);
+
+export default App;
